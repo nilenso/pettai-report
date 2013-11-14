21dd5d894ab76d0a8c6e5a8cdd983f6105dbede4
Tejas Dinkar <tejas@gja.in>
2013-11-14 23:01:10 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PettaiReport::Application.config.secret_key_base = '4e4ab285a914c47d2ef3cb35379a2a23f87327bd52ad875812a96ca78d52e8b481bd67921e0bec0ea6fc762a320538a57fa2415646b5c640bdfe59c74c75bd63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PettaiReport::Application.config.session_store :cookie_store, key: '_pettai-report_session'
